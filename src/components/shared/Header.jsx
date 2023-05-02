9989bfab53e6fdefaf6fc3b163696ac465d0fe25
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-05-02 21:04:39 +0600

    header footer added

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -6,10 +6,16 @@ const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home" className='h-10'>
-                    <img className="img-fluid" src="" />
-                    Navbar
-                </Navbar.Brand>
+            <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://raw.githubusercontent.com/SumonaShimu/FoodData/main/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            MuriGhonto
+          </Navbar.Brand>
                 <Nav className="d-flex gap-4">
                     <Link to='/' className='text-decoration-none'>Home</Link>
                     <Link to='/login' className='text-decoration-none'>Login</Link>
