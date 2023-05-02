10998c89870f160220f35153405be34c83685e53
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-05-03 01:02:51 +0600

    about us modified

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -3,14 +3,14 @@ import { Button, Container, Image } from 'react-bootstrap';
 
 const Aboutus = () => {
     return (
-        <Container className='about ' style={{height:'90vh'}}>
+        <Container className='about ' style={{minHeight:'90vh'}}>
             <div className='my-auto'>
                 <h1 className='banner-title py-5'>About Us</h1>
                 <p>Conveniently imaiipact are worldwide and datang arovem theme with there. Conveniently imaiipact are worldwide.Conveniently imaiipact are worldwide and datang arovem theme with there. Conveniently imaiipact are worldwide.</p>
                 <Button variant="warning">Contact Us</Button>
             </div>
             <div className='my-auto ' style={{height:'100%',width:'100%'}}>
-            <img src="https://raw.githubusercontent.com/SumonaShimu/FoodData/main/pan.jpg" className='img-fit'/>
+            <img src="https://raw.githubusercontent.com/SumonaShimu/FoodData/main/eat.gif" className='img-fit'/>
             </div>
             
         </Container>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,15 +3,13 @@ import { Button, Container } from 'react-bootstrap';
 
 const Banner = () => {
     return (
-        <div className="banner-container">
-            <div className="banner-overlay d-flex align-items-center justify-content-center">
-                <div className="banner-text w-50 d-block">
-                    <h4 className='text-warning'>Welcome to MuriGhonto</h4>
-                    <h1 className='banner-title'>Get the recepies <br></br>Make The Food You Love</h1>
-                    <p >Conveniently imaiipact are worldwide andng datang arovem theme with there.Conveniently imaiipact are worldwide andng datang arovem theme with there.Conveniently imaiipact are worldwide andng datang arovem theme with there</p>
-                    <Button variant="outline-warning">Get Satrted</Button>
-                    <Button variant="warning" className='m-3'>Whats New Today?</Button>
-                </div>
+        <div className="banner-container d-flex align-items-center justify-content-center">
+            <div className="banner-text w-50 d-block">
+                <h4 className='text-warning'>Welcome to MuriGhonto</h4>
+                <h1 className='banner-title'>Get the recepies <br></br>Make The Food You Love</h1>
+                <p >Conveniently imaiipact are worldwide andng datang arovem theme with there.Conveniently imaiipact are worldwide andng datang arovem theme with there.Conveniently imaiipact are worldwide andng datang arovem theme with there</p>
+                <Button variant="outline-warning">Get Satrted</Button>
+                <Button variant="warning" className='m-3'>Whats New Today?</Button>
             </div>
         </div>
     );
