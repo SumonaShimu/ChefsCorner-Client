f95db2b751cbc844b3a20498fe56f907af4974a5
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-05-05 23:12:31 +0600

    complete

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { SlSocialInstagram, SlSocialTwitter, SlSocialYoutube } from "react-icons/sl";
 const Footer = () => {
     return (
-        <div className='d-flex justify-content-between align-items-center p-4 w-100 bg-dark text-white'>
+        <div className='bg-dark' style={{fontSize:'30px'}}>
+        <Container className='d-flex justify-content-between align-items-center p-4 w-100 text-white'>
             <h6>&copy;2023 MuriGhonto. All Rights Reserved</h6>
             <div>
-            <div className='d-flex gap-2'>
+            <div className='d-flex gap-2 text-center'>
             <SlSocialYoutube></SlSocialYoutube>
             <SlSocialYoutube></SlSocialYoutube>
             <SlSocialTwitter></SlSocialTwitter>
             <SlSocialInstagram></SlSocialInstagram>
             </div>
-            <h6>Connect with us</h6>
+            <h6 className='text-center'>Connect with us</h6>
             </div>
+        </Container>
         </div>
     );
 };
